fbf6b7b15f972aded4b594be1e7b71065778b379
Alvaro Augusto Perdigao Candido Fernandes <fernandes.alvaro@gmail.com>
2025-02-21 19:26:44 +1100

    navbar created

---
+++ components/Navbar.tsx
@@ -0,0 +1,90 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetTitle,
+  SheetDescription,
+} from "./ui/sheet";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { Landmark, MenuIcon } from "lucide-react";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+
+export const Navbar = () => {
+  return (
+    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="lg:hidden">
+            <MenuIcon className="h-6 w-6" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" aria-describedby={undefined}>
+          <SheetTitle>
+            <p>Personal Finance App</p>
+          </SheetTitle>
+          <Link href="/" className="mr-6 hidden lg:flex" prefetch={false}>
+            <Landmark className="w-6 h-6" />
+            <span className="sr-only">Personal Finance App</span>
+          </Link>
+          <div className="grid gap-2 py-6">
+            <Link
+              href="/"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Home
+            </Link>
+            <Link
+              href="/auth/signin"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Signin
+            </Link>
+            <Link
+              href="/auth/signup"
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              prefetch={false}
+            >
+              Signup
+            </Link>
+          </div>
+          <VisuallyHidden>
+            <SheetDescription>
+              App for my personal finance control
+            </SheetDescription>
+          </VisuallyHidden>
+        </SheetContent>
+      </Sheet>
+      <Link href="/" className="mr-6 hidden lg:flex">
+        <Landmark className="w-6 h-6" />
+        <span className="sr-only">Personal Finance App</span>
+      </Link>
+      <nav className="ml-auto hidden lg:flex gap-6">
+        <Link
+          href="/"
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          prefetch={false}
+        >
+          Home
+        </Link>
+        <Link
+          href="/auth/signin"
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          prefetch={false}
+        >
+          Signin
+        </Link>
+        <Link
+          href="/auth/signup"
+          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+          prefetch={false}
+        >
+          Signup
+        </Link>
+      </nav>
+    </header>
+  );
+};
